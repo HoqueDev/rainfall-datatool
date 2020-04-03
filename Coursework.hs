869a0bf49c8e440d6c4675feefdefdc387b8aee9
HoqueDev <38895062+HoqueDev@users.noreply.github.com>
2020-04-03 17:44:52 +0100

    Task 4 completed

--- Coursework.hs
+++ Coursework.hs
@@ -22,7 +22,7 @@ testData = [("London", 51.5, -0.1, 0, 0, 5, 8, 8, 0, 0),
             ("Lerwick", 60.2, -1.1, 8, 10, 5, 5, 0, 0, 3),
             ("St Helier", 49.2, -2.1, 0, 0, 0, 0, 6, 10, 0)]
 
----------------------------------------- Task 1 --------------------------------------------------
+---------------------------------------- Task 1 -----------------------------------------------
 -- Use: > displayNames testData
 
 displayNames :: [Place] -> [String]
@@ -30,7 +30,7 @@ displayNames nameList = [p | (p, n, s, a, b, c, d, e, f, g) <- nameList ]
 
 
 
----------------------------------------- Task 2---------------------------------------------------
+---------------------------------------- Task 2------------------------------------------------
 -- Use: > getAverage testData "London"
 
 getAverage :: [Place] -> String -> Float
@@ -45,7 +45,7 @@ getAverage (p:ps) find
         avg xs = (fi . sum $ xs) / (fi . length $ xs)
 
 
---------------------------------------- Task 3 ----------------------------------------------------
+--------------------------------------- Task 3 -------------------------------------------------
 -- Use: > putStrLn(placesToString testData)
 
 fillLeft c n s = s ++ replicate (n - length s) c
@@ -57,19 +57,57 @@ placesToString places = unlines [intercalate "  |  " [fillLeft ' ' 10 location,
                                                       fillLeft ' ' 3(show wed),
                                                       fillLeft ' ' 3(show thu),
                                                       fillLeft ' ' 3(show fri),
-													  fillLeft ' ' 3(show sat), 
-													  fillLeft ' ' 3(show sun)]
+                                                      fillLeft ' ' 3(show sat), 
+                                                      fillLeft ' ' 3(show sun)]
                                                       | (location, _, _ , mon, tue, wed, thu, fri, sat, sun ) <- places ]
 
 								
 --------------------------------------- Task 4 ----------------------------------------------------
 
-getDryPlaces :: [Place] -> Int -> String
+--getDryPlaces :: [Place] -> Int -> [String]
+--getDryPlaces testData days = [location | (location, _, _, mon, tue, wed, thu, fri, sat, sun) <- testData ]
+
+--return a list of the names of places that were totally dry (i.e. had zero rainfall) a given
+--number of days ago (1 meaning yesterday, 7 meaning a week ago)
+
+--days:
+--1 = sun
+--2 == sat
+--3 = mon
+--4 =  tue
+
+getDay :: Int -> String
+getDay x
+  | x == 1 = "sun"
+  | x == 2 = "sat"
+  | x == 3 = "fri"
+  | x == 4 = "thu"
+  | x == 5 = "wed"
+  | x == 6 = "tue"
+  | x == 7 = "mon"
+  
+
+getDryDays :: [Place] -> Int -> [String]
+getDryDays testData daysAgo = [ name | (name, _ , _, mon, tue, wed, thu, fri, sat, sun) <- testData, 
+                                                                                                     daysAgo == 7 && sun == 0 || 
+																									 daysAgo == 6 && sat == 0 ||
+																									 daysAgo == 5 && fri == 0 ||
+																									 daysAgo == 4 && thu == 0 ||
+																									 daysAgo == 3 && wed == 0 ||
+																									 daysAgo == 2 && tue == 0 ||
+																									 daysAgo == 1 && mon == 0]
+
+						
 
 
+		
 
+		
+		
+		
+		
 
-
+						  
 
 
 
