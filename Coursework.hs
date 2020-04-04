79b4a90313d534c078c9962081738fe257f2cc71
HoqueDev <38895062+HoqueDev@users.noreply.github.com>
2020-04-05 00:34:32 +0100

    Task 5 Completed

--- Coursework.hs
+++ Coursework.hs
@@ -2,7 +2,10 @@
 
 import Data.List
 import System.IO
-import Text.Printf
+
+
+
+
 
 type Place = (String, Double, Double, Int, Int, Int, Int, Int, Int ,Int)
 testData :: [Place]
@@ -21,23 +24,24 @@ testData = [("London", 51.5, -0.1, 0, 0, 5, 8, 8, 0, 0),
             ("Stornoway", 58.2, -6.4, 15, 6, 15, 0, 0, 4, 2),
             ("Lerwick", 60.2, -1.1, 8, 10, 5, 5, 0, 0, 3),
             ("St Helier", 49.2, -2.1, 0, 0, 0, 0, 6, 10, 0)]
+		 
+		 
 
 ---------------------------------------- Task 1 -----------------------------------------------
 -- Use: > displayNames testData
 
 displayNames :: [Place] -> [String]
-displayNames nameList = [p | (p, n, s, a, b, c, d, e, f, g) <- nameList ]
-
+displayNames nameList = [name | (name, _, _, _, _, _, _, _, _, _) <- nameList ]
 
 
 ---------------------------------------- Task 2------------------------------------------------
--- Use: > getAverage testData "London"
+-- Use: > returnAverage testData "London"
 
-getAverage :: [Place] -> String -> Float
-getAverage [] _ = 0
-getAverage (p:ps) find
+returnAverage :: [Place] -> String -> Float
+returnAverage [] _ = 0
+returnAverage (p:ps) find
     | find == name = avg [a,b,c,d,e,f,g]
-    | otherwise = getAverage ps find
+    | otherwise = returnAverage ps find
     where
         (name, _,_ , a,b,c,d,e,f,g) = p
         fi = fromIntegral 
@@ -45,8 +49,9 @@ getAverage (p:ps) find
         avg xs = (fi . sum $ xs) / (fi . length $ xs)
 
 
+		
 --------------------------------------- Task 3 -------------------------------------------------
--- Use: > putStrLn(placesToString testData)
+-- Usage: > putStrLn(placesToString testData)
 
 fillLeft c n s = s ++ replicate (n - length s) c
 
@@ -59,36 +64,15 @@ placesToString places = unlines [intercalate "  |  " [fillLeft ' ' 10 location,
                                                       fillLeft ' ' 3(show fri),
                                                       fillLeft ' ' 3(show sat), 
                                                       fillLeft ' ' 3(show sun)]
-                                                      | (location, _, _ , mon, tue, wed, thu, fri, sat, sun ) <- places ]
+                                                      | (location, _, _, mon, tue, wed, thu, fri, sat, sun ) <- places ]
 
 								
+								
 --------------------------------------- Task 4 ----------------------------------------------------
+-- Usage: > returnDryDays testData daysAgo
 
---getDryPlaces :: [Place] -> Int -> [String]
---getDryPlaces testData days = [location | (location, _, _, mon, tue, wed, thu, fri, sat, sun) <- testData ]
-
---return a list of the names of places that were totally dry (i.e. had zero rainfall) a given
---number of days ago (1 meaning yesterday, 7 meaning a week ago)
-
---days:
---1 = sun
---2 == sat
---3 = mon
---4 =  tue
-
-getDay :: Int -> String
-getDay x
-  | x == 1 = "sun"
-  | x == 2 = "sat"
-  | x == 3 = "fri"
-  | x == 4 = "thu"
-  | x == 5 = "wed"
-  | x == 6 = "tue"
-  | x == 7 = "mon"
-  
-
-getDryDays :: [Place] -> Int -> [String]
-getDryDays testData daysAgo = [ name | (name, _ , _, mon, tue, wed, thu, fri, sat, sun) <- testData, 
+returnDryDays :: [Place] -> Int -> [String]
+returnDryDays testData daysAgo = [ name | (name, _, _, mon, tue, wed, thu, fri, sat, sun) <- testData, 
                                                                                                      daysAgo == 7 && sun == 0 || 
 																									 daysAgo == 6 && sat == 0 ||
 																									 daysAgo == 5 && fri == 0 ||
@@ -96,23 +80,44 @@ getDryDays testData daysAgo = [ name | (name, _ , _, mon, tue, wed, thu, fri, sa
 																									 daysAgo == 3 && wed == 0 ||
 																									 daysAgo == 2 && tue == 0 ||
 																									 daysAgo == 1 && mon == 0]
+																					
+																			
+																			
+--------------------------------------- Task 5 ----------------------------------------------------
+-- Usage: > updateAll [] testData [22,22,22,22,22,22,22,22,22,22,22,22,22,22]
 
-						
-
-
+updateAll :: [Place] -> [Place] -> [Int] -> [Place]
+updateAll newPlaces testData nums 
+        | (length newPlaces) < 14 = updateAll (newPlaces ++ [(updateData (testData!!x) (nums!!x))]) testData nums
+        | otherwise               = newPlaces
+        where x = (length newPlaces)
 		
 
-		
-		
-		
-		
-
-						  
+updateData :: Place -> Int -> Place
+updateData (place, north, east, d1, d2, d3, d4, d5, d6, d7) newVal = (place, north, east, newVal, d1, d2, d3, d4, d5, d6)
 
 
 
+--------------------------------------- Task 6 -----------------------------------------------------
+
+-- vi. replace a given existing place with a new place (the new place will have a name, a
+-- location and a list of 7 rainfall figures)
+
+-- Usage: > updatePlace [] testData [("Portsmouth" ,50.5,30.2,1,2,3,4,5,6,7)]
+
+updatePlace :: [Place] -> [Place] -> [Place] -> [Place]
+updatePlace newPlaces testData newRecord 
+         | (length newPlaces) < 14 = updatePlace (newPlaces ++ [(updateRecord (testData!!x) (newRecord!!x))]) testData newRecord
+         | otherwise               = newPlaces
+         where x = (length newPlaces)
 
 
+updateRecord :: Place -> Place -> Place
+updateRecord (place, north, east, d1, d2, d3, d4, d5, d6, d7) newRecord = (place, north, east, d1, d2, d3, d4, d5, d6, d7)
+
+
+
+--------------------------------------- Task 7 -----------------------------------------------------
 
 
 
